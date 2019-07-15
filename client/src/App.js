a1a553756f8c11b6944a89a81d817fcc228e2012
breadracer <breadracer@gmail.com>
2019-07-15 22:05:08 +0800

    Expereimental version with cookies

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,120 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import axios from 'axios';
+import cookie from 'cookie';
 import './App.css';
+
+// import Login from './Login'
+// import Register from './Register'
+// import Home from './Home'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       socket: null,
-      connected: false
+      connected: false,
+      token: cookie.parse(document.cookie).access_token || '',
+      username: '',
+      password: ''
     };
   }
 
   componentDidMount() {
-    const socket = new WebSocket('ws://localhost:8000');
+    // Try connect with current cookie sent in request param
+    // If auth success, connect to ws server
+    // If no cookie, go to Login page
+
+    const socket = new WebSocket(
+      `ws://localhost:8000/?access_token=${this.state.token}`);
+    
     this.setState({ socket });
     socket.onopen = _ => {
       console.log('Connected to breadracer.com');
       this.setState({ connected: true });
     };
     socket.onclose = _ => {
-      console.log('Disconnected to breadracer.com');
+      console.log('Cannot connect or disconnected to breadracer.com');
+      this.setState({ connected: false });
     };
     socket.onmessage = event => {
       console.log(`Roundtrip time: ${Date.now() - event.data} ms`);
     };
   }
 
-  handleSubmit = () => {
-    if (this.state.connected)
+  handleTestSpeed = (e) => {
+    if (this.state.connected) {
       this.state.socket.send(Date.now());
-    else
+    } else {
       console.log('Not connected yet');
+    }
   }
 
+  handleSubmitLogin = (e) => {
+      e.preventDefault();
+
+      axios.post('http://localhost:8000/api/login', {
+        headers: { 
+          crossDomain: true,
+          withCredentials: true,
+          Content_Type: 'application/json'
+        },
+        username: this.state.username,
+        password: this.state.password
+      }).then(res => {
+        console.log(res.data);
+      }).catch(err => {
+        console.log(err);
+      });
+
+      this.setState({
+        username: '',
+        password: ''
+      })
+  }
+
+  onChange = e => { this.setState({[e.target.name]: e.target.value}); }
+
   render() {
+    const title = this.state.connected ? (
+      <h1>You are logged in!</h1>) : (
+      <h1>Please log in first!</h1>
+    );
+
     return (
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route exact path='/' component={Home} />
+      //     <Route exact path='/register' component={Register} />
+      //     <Route exact path='/login' component={Login} />
+      //   </Switch>
+      // </BrowserRouter>
+
       <div>
-        <h1>Welcome to the homepage!</h1>
-        <button onClick={this.handleSubmit}>Sumbit message!</button>
+        {title}
+        <button onClick={this.handleTestSpeed}>
+          Get ws roundtrip time
+        </button>
+
+        {/* <form action='http://breadracer.com/register' method='POST'>
+          <input type='text' title='username' name='username' />
+          <input type='text' title='password' name='password' />
+          <button onClick={this.handleSubmit.bind(this, 'register')}>
+            Sign me up
+          </button>
+        </form> */}
+  
+        <form>
+          <input type='text' title='username' onChange={this.onChange}
+            name='username' value={this.state.username} />
+          <input type='text' title='password' onChange={this.onChange}
+            name='password' value={this.state.password} />
+          <button onClick={this.handleSubmitLogin}>
+            Log me in
+          </button>
+        </form>
+
+
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,83 @@
+const http = require('http');
+const url = require('url');
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ port: 8000 });
-server.on('connection', ws => {
-  ws.on('message', msg => {
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, GET',
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Max-Age': 2592000, // 30 days
+  'Access-Control-Allow-Credentials': true,
+  'Content-Type': 'application/json'
+};
+
+let users = [
+  {
+    username: 'root',
+    password: '123'
+  }
+]
+
+let connections = []
+
+let handleRegister = (req, res) => {
+  console.log('request to /api/register');
+}
+
+let handleLogin = (req, res) => {
+  if (req.method === 'POST') {
+    let requestData = '';
+    req.on('data', chunk => { requestData += chunk.toString(); });
+    req.on('end', () => {
+      console.log(requestData);
+
+      // TODO: Verify user information
+      if (true) {
+        res.writeHead(200, 'OK', {
+          'Set-Cookie': 'access_token=abc',
+          ...headers
+        });
+        res.end(JSON.stringify({as: 'as'}));
+      }
+    })
+  }
+  console.log('request to /api/login');
+}
+
+
+let requestListener = (req, res) => {
+  let query = url.parse(req.url, true);
+
+
+  switch(query.pathname) {
+    case '/api/register': handleRegister(req, res); break;
+    case '/api/login': handleLogin(req, res); break;
+    default: console.log('invalid request path');
+  }
+}
+
+const server = http.createServer(requestListener);
+
+let verifyClient = (info, callback) => {
+  let token = url.parse(info.req.url, true).query.access_token;
+  
+  // TODO: Verify the token
+  if (token && token === 'abc') {
+    callback(true);
+  } else {
+    callback(false, 401, 'Unauthorized');
+  }
+}
+
+const webSocketServer = new WebSocket.Server({ server, verifyClient });
+webSocketServer.on('connection', socket => {
+
+  socket.on('message', msg => {
     console.log('received: %s', msg);
-    ws.send(msg);
+    socket.send(msg);
   });
+
 });
+
+server.listen(8000);
 
