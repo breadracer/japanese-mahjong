c5aa09e3c612dad28a929fae8bac8d16007ef963
breadracer <breadracer@gmail.com>
2019-07-16 23:16:37 +0800

    Basic sign up and log in

--- client/src/App.js
+++ client/src/App.js
@@ -16,33 +16,45 @@ class App extends React.Component {
       connected: false,
       token: cookie.parse(document.cookie).access_token || '',
       username: '',
-      password: ''
+      password: '',
+      // TODO: More on this later
+      // loggedUser: null
     };
   }
 
   componentDidMount() {
+    
     // Try connect with current cookie sent in request param
     // If auth success, connect to ws server
     // If no cookie, go to Login page
+    this.webSocketTryConnect(null);
 
-    const socket = new WebSocket(
-      `ws://breadracer.com:8000/?access_token=${this.state.token}`);
-    
-    this.setState({ socket });
-    socket.onopen = _ => {
-      console.log('Connected to breadracer.com');
-      this.setState({ connected: true });
-    };
-    socket.onclose = _ => {
-      console.log('Cannot connect or disconnected to breadracer.com');
-      this.setState({ connected: false });
-    };
-    socket.onmessage = event => {
-      console.log(`Roundtrip time: ${Date.now() - event.data} ms`);
-    };
   }
 
-  handleTestSpeed = (e) => {
+  webSocketTryConnect = _ => {
+    if (!this.state.socket) {
+      const socket = new WebSocket(
+        `ws://localhost:8000/?access_token=${
+        cookie.parse(document.cookie).access_token}`);
+  
+      this.setState({ socket });
+      socket.onopen = _ => {
+        console.log('Connected to localhost');
+        this.setState({ connected: true });
+      };
+      socket.onclose = _ => {
+        console.log('Cannot connect or disconnected to localhost');
+        this.setState({ connected: false, socket: null });
+      };
+      socket.onmessage = event => {
+        console.log(`Roundtrip time: ${Date.now() - event.data} ms`);
+      };
+    } else {
+      console.log('Already connected');
+    }
+  }
+
+  handleTestSpeed = _ => {
     if (this.state.connected) {
       this.state.socket.send(Date.now());
     } else {
@@ -50,13 +62,13 @@ class App extends React.Component {
     }
   }
 
-  handleSubmitLogin = (e) => {
-      e.preventDefault();
+  handleRegister = e => {
+    e.preventDefault();
 
-      axios.post('http://breadracer.com:8000/api/login', {
-        username: this.state.username,
-        password: this.state.password
-      }, {
+    axios.post('http://localhost:8000/api/register', {
+      username: this.state.username,
+      password: this.state.password
+    }, {
         headers: {
           'Cross-Domain': true,
           'Content-Type': 'application/json'
@@ -68,19 +80,70 @@ class App extends React.Component {
         console.log(err);
       });
 
-      this.setState({
-        username: '',
-        password: ''
-      })
+    this.setState({
+      username: '',
+      password: ''
+    })
   }
 
-  onChange = e => { this.setState({[e.target.name]: e.target.value}); }
+  handleLogin = e => {
+    e.preventDefault();
+
+    axios.post('http://localhost:8000/api/login', {
+      username: this.state.username,
+      password: this.state.password
+    }, {
+        headers: {
+          'Cross-Domain': true,
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }).then(res => {
+        console.log(res.data);
+        this.setState({ loggedUser: res.data.user });
+        this.webSocketTryConnect(null);
+      }).catch(err => {
+        console.log(err);
+      });
+
+    this.setState({
+      username: '',
+      password: ''
+    })
+  }
+
+  handleLogout = _ => {
+    // axios.post('http://localhost:8000/api/logout', {
+    //   username: this.state.loggedUser,
+    // }, {
+    //     headers: {
+    //       'Cross-Domain': true,
+    //       'Content-Type': 'application/json'
+    //     },
+    //     withCredentials: true
+    //   }).then(res => {
+    //     console.log(res.data);
+    //     this.state.socket.close();
+    //     document.cookie = '';
+    //     this.setState({ loggedUser: null, socket: null });
+    //   }).catch(err => {
+    //     console.log(err);
+    //   });
+  }
+
+  onChange = e => { this.setState({ [e.target.name]: e.target.value }); }
 
   render() {
     const title = this.state.connected ? (
-      <h1>You are logged in!</h1>) : (
-      <h1>Please log in first!</h1>
-    );
+      <h1>You are connected!</h1>) : (
+        <h1>Please try connect first!</h1>
+      );
+
+    // const userWelcome = this.state.loggedUser ? (
+    //   <h1>Hi, {this.state.loggedUser}!</h1>
+    // ) : (
+    //   <h1>You are not logged in</h1>
+    // )
 
     return (
       // <BrowserRouter>
@@ -93,27 +156,28 @@ class App extends React.Component {
 
       <div>
         {title}
-        <button onClick={this.handleTestSpeed}>
-          Get ws roundtrip time
-        </button>
+        <button onClick={this.handleTestSpeed}>Get ws roundtrip time</button>
+        <button onClick={this.webSocketTryConnect}>Try reconnecting</button>
 
-        {/* <form action='http://breadracer.com/register' method='POST'>
+        {/* <form action='http://localhost/register' method='POST'>
           <input type='text' title='username' name='username' />
           <input type='text' title='password' name='password' />
           <button onClick={this.handleSubmit.bind(this, 'register')}>
             Sign me up
           </button>
         </form> */}
-  
+
         <form>
           <input type='text' title='username' onChange={this.onChange}
             name='username' value={this.state.username} />
-          <input type='text' title='password' onChange={this.onChange}
+          <input type='password' title='password' onChange={this.onChange}
             name='password' value={this.state.password} />
-          <button onClick={this.handleSubmitLogin}>
-            Log me in
-          </button>
+          <button onClick={this.handleLogin}>Sign in</button>
+          <button onClick={this.handleRegister}>Sign up</button>
         </form>
+
+        <button onClick={this.handleLogout}>Sign out</button>
+
 
 
       </div>
--- server/server.js
+++ server/server.js
@@ -3,83 +3,155 @@
 const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const headers = {
   'Access-Control-Allow-Origin': 'http://breadracer.com',
   'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-  'Access-Control-Allow-Headers': 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie, Cross-Domain',
-  'Access-Control-Max-Age': 2592000, // 30 days
+  'Access-Control-Allow-Headers':
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, ' +
+    'Accept, Set-Cookie, Cross-Domain',
+  'Access-Control-Max-Age': 86400, // 24 hours
   'Access-Control-Allow-Credentials': true,
   'Content-Type': 'application/json'
 };
 
-let users = [
-  {
-    username: 'root',
-    password: '123'
-  }
-]
+// TODO: Set this to some environment variable
+const SECRET_KEY = 'SECRET_KEY';
 
-let connections = []
+let users = [];
 
-let handleRegister = (req, res) => {
-  console.log('request to /api/register');
-}
+// TODO: Create a map between these two
+let sessions = [];
+let connections = [];
 
-let handleLogin = (req, res) => {
-  console.log(req.method)
+// let handleRegister = ([req, res]) => {
+//   console.log('request to /api/register');
+
+//   if ()
+
+// }
+
+// let handleLogin = ([req, res]) => {
+//   // TODO: Verify user information
+//   if (true) {
+//     res.writeHead(200, 'OK', {
+//       'Set-Cookie': 'access_token=abc; Path=/',
+//       ...headers
+//     });
+//     res.write(JSON.stringify({ as: 'as' }));
+//     res.end();
+//   }
+//   console.log('request to /api/login');
+// }
+
+
+function requestListener(req, res) {
+  let query = url.parse(req.url, true);
+
   if (req.method === 'OPTIONS') {
     console.log('OPTIONS success');
-    res.writeHead(200, 'OK', headers);
+    res.writeHead(200, headers);
     res.end();
-  } else if (req.method === 'POST') {
+  } else if (req.method == 'POST') {
+    // Handle data stream
     let requestData = '';
     req.on('data', chunk => { requestData += chunk.toString(); });
     req.on('end', () => {
-      console.log(requestData);
-
-      // TODO: Verify user information
-      if (true) {
-        res.writeHead(200, 'OK', {
-          'Set-Cookie': 'access_token=abc; Path=/',
-          ...headers
-        });
-        res.write(JSON.stringify({as: 'as'}));
-        res.end();
+      let requestBody = JSON.parse(requestData);
+      console.log(requestBody);
+      switch (query.pathname) {
+        case '/api/register': {
+          // Check if the user already exists
+          // TODO: Store users in the database rather than in RAM
+          if (users.some(e => e.username === requestBody.username)) {
+            res.writeHead(400, 'Bad Request', headers);
+            res.end(JSON.stringify({ message: 'User already registered' }));
+          } else {
+            let hash = bcrypt.hashSync(requestBody.password, 10);
+            users.push({ username: requestBody.username, hash });
+            console.log(users);
+            res.writeHead(201, 'Created', headers);
+            res.end(JSON.stringify({ message: 'Successfully signed up' }));
+          }
+          break;
+        }
+        case '/api/login': {
+          let user = users.find(e => e.username === requestBody.username);
+          if (user && bcrypt.compareSync(requestBody.password, user.hash)) {
+            // Generate JWT token
+            let token = jwt.sign({
+              username: user.username
+            }, SECRET_KEY, {
+                expiresIn: '24h'
+              });
+            console.log(token);
+            res.writeHead(200, 'OK', {
+              'Set-Cookie': `access_token=${token}; Path=/`,
+              ...headers
+            });
+            res.end(JSON.stringify({
+              message: 'Successfully signed in',
+              user: user.username
+            }));
+            // sessions.push(user.username);
+            // console.log(sessions);
+          } else {
+            res.writeHead(401, 'Unauthorized', headers);
+            res.end(JSON.stringify({
+              message: 'Incorrect username or password'
+            }));
+          }
+          break;
+        }
+        // case '/api/logout': {
+        //   let index = sessions.indexOf(requestBody.username);
+        //   if (index > -1)
+        //     sessions.splice(index, 1);
+        //   res.writeHead(200, 'OK', headers);
+        //   res.end(JSON.stringify({
+        //     message: `Bye ${requestBody.username}!`,
+        //   }));
+        //   break;
+        // }
+        default: console.log('invalid request path');
       }
-    })
+    });
   }
-  console.log('request to /api/login');
-}
 
 
-let requestListener = (req, res) => {
-  let query = url.parse(req.url, true);
-
-
-  switch(query.pathname) {
-    case '/api/register': handleRegister(req, res); break;
-    case '/api/login': handleLogin(req, res); break;
-    default: console.log('invalid request path');
-  }
 }
 
 const server = http.createServer(requestListener);
 
 let verifyClient = (info, callback) => {
-  let token = url.parse(info.req.url, true).query.access_token;
-  
-  // TODO: Verify the token
-  if (token && token === 'abc') {
-    callback(true);
-  } else {
+  let token = url.parse(info.req.url, true).query.access_token || '';
+  let decoded = {};
+  try {
+    decoded = jwt.verify(token, SECRET_KEY);
+  } catch (err) {
+    console.log(err);
     callback(false, 401, 'Unauthorized');
   }
+  callback(true);
 }
 
 const webSocketServer = new WebSocket.Server({ server, verifyClient });
 webSocketServer.on('connection', socket => {
-
+  // socket.on('open', () => {
+  //   console.log(`A new connection is established`);
+  //   connections.push(socket);
+  //   console.log(connections);
+  //   socket.send(`Hello, client #${connections.length}`);
+  // })
+  // socket.on('close', () => {
+  //   console.log(`A connection is closed`);
+  //   let index = connections.indexOf(socket);
+  //   if (index > -1)
+  //     connections.splice(index, 1);
+  //   console.log(connections);
+  // })
   socket.on('message', msg => {
     console.log('received: %s', msg);
     socket.send(msg);
